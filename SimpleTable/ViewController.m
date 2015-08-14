ab4a1c428bc35e9ef21652d8edacd942777129a9
Zhu Dengquan <dqzhu@ideabinder.com>
2015-08-14 22:46:12 +0800

    Using the property list file to store the table date
    the property plist file is used to store the some small amount of data
    (e.g setting the app )

--- SimpleTable/ViewController.m
+++ SimpleTable/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 #import "SimpleTableCell.h"
 
+#define RecipeName @"RecipeName"
+#define Thumbnail @"Thumbnail"
+#define PrepTime @"PrepTime"
+
 @interface ViewController ()
 
 {
@@ -25,21 +29,13 @@
 {
     [super viewDidLoad];
     
-    recipes = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast",
-               @"Humburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"Whilte Chocolate Donut",
-               @"StarBucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg",
-               @"Noodle with BBQ pork", @"Japanese Noodle with Pork", @"Green Tea",
-               @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"recipes" ofType:@"plist"];
     
-    thumbnails = [NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg",
-                  @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg",
-                  @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg",
-                  @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg",
-                  @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg",
-                  @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
+    NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:path];
     
-    prepTime = [NSArray arrayWithObjects:@"30 min", @"28 min", @"29 min", @"25 min", @"31 min", @"35 min", @"26 min",
-                @"30 min", @"28 min", @"29 min", @"25 min", @"31 min", @"35 min", @"26 min",@"30 min", @"28 min", nil];
+    recipes = [dict valueForKey:RecipeName];
+    thumbnails = [dict valueForKey:Thumbnail];
+    prepTime = [dict valueForKey:PrepTime];
 }
 
 - (void)didReceiveMemoryWarning
